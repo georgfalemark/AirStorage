f0de7883962701926de380908c71b1c20e91f499
georgfalemark2 <ge2117fa-s@student.lu.se>
2019-11-27 12:54:26 +0100

    nestled _layout added

--- Domain/Models/Entities/Storage.cs
+++ Domain/Models/Entities/Storage.cs
@@ -8,5 +8,60 @@ namespace Domain.Models.Entities
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+
+
+
+        public decimal FloorLength { get; set; }
+
+        public decimal FloorWidth { get; set; }
+
+        //or
+
+
+        public decimal Heigth { get; set; }
+
+
+
+        public decimal SquareMeter { get; set; }
+
+
+
+
+        public decimal CubicMeter { get; set; }
+
+
+
+        public string Location { get; set; }
+
+
+
+
+
+        public string NameOfOwner { get; set; }
+
+
+
+
+        public string Email { get; set; }
+
+
+
+        public string PhoneNumber { get; set; }
+
+
+
+        public string Description { get; set; }
+
+
+
+        public decimal PricePerMonth { get; set; }
+
+
+
+
+
+
+
+
     }
 }
