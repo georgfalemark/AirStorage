4b204bfbe2fb3e1afcf775961bfde3f677a86ee5
georgfalemark2 <ge2117fa-s@student.lu.se>
2020-02-28 23:45:19 +0100

    changed the data-folder slightly

--- AirStorage/Startup.cs
+++ AirStorage/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AirStorage.Data;
+using AirStorage.Data.DbContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
